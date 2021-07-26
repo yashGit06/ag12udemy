0c56043a54053c00b76e6a498034431c5c359be6
yashGit06 <yashwantkumarsahu2014@gmail.com>
2021-07-26 22:50:09 +0530

    ngfor ngstyle ngclass L43

--- first-ag12App/src/app/server/server.component.ts
+++ first-ag12App/src/app/server/server.component.ts
@@ -2,17 +2,29 @@ import { Component } from "@angular/core";
 
 @Component({
     selector:'app-server',
-    templateUrl:'./server.component.html'
+    templateUrl:'./server.component.html',
+    styles:[`.online{
+        color: white;
+    }
+    .offline{
+        color: black;
+    }`]
 })
 
 export class ServerComponent{
-    randomeValue = Math.random()
+    randomeValue = Math.random();
     serverStatus : string;
     constructor(){
-        this.serverStatus=this.randomeValue>0.5?"online":"offline";
+        //this.serverStatus=this.randomeValue>0.5?"online":"offline";
+        this.onRefresh();
     }
 
     onColorChange(){
         return this.serverStatus==='online'?"green":"red";
+    }
+
+    onRefresh(){
+        this.randomeValue= Math.random();
+        this.serverStatus=this.randomeValue>0.5?"online":"offline";
     }
 }
--- first-ag12App/src/app/servers/servers.component.ts
+++ first-ag12App/src/app/servers/servers.component.ts
@@ -6,15 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-  serverName="";
+  serverName="TestServer";
   evtBind="";
+  myServers=['s1','s2'];
 
   constructor() { }
 
   ngOnInit(): void {
   }
-  onCreateText(event:Event){
+  onCreateText(event:Event){  
     this.evtBind=(<HTMLInputElement>event.target).value;
+  }
+
+  onCreateServers(){
+    this.myServers.push(this.serverName+Math.ceil(Math.random()*10)+1);
   }
 
 }
